ce5e05a4e0c28d149b58285c659c076d720d64ca
Deiiivy <deivypr28@gmail.com>
2025-04-02 11:45:14 -0500

    Agregada clase animal

---
+++ ZooManagement/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooManagement.Modelos
+{
+    public abstract class Animal
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Animal(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public abstract string HacerSonido();
+    }
+}
+
